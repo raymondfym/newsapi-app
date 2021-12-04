bd7fd4e782927f5523214c4c4e0895e18ba201a3
raymondfy14 <raymondfarazmalau@gmail.com>
2021-12-04 18:16:35 +0700

    Minor Changes
    icon and color scheme

--- app/src/main/java/com/example/newsapi_app/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/newsapi_app/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.newsapi_app.ui.notifications;
+package com.example.newsapi_app.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,24 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.newsapi_app.R;
-import com.example.newsapi_app.databinding.FragmentNotificationsBinding;
+import com.example.newsapi_app.databinding.FragmentProfileBinding;
 
-public class NotificationsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-    private FragmentNotificationsBinding binding;
+    private ProfileViewModel profileViewModel;
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView textView = binding.textProfile;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
